33f464fed8398c5cbb9c698319f0fe5e2052ad86
d0ct0rstrange <49350390+d0ct0rstrange@users.noreply.github.com>
2022-03-31 20:08:44 +0530

    Updated description

--- cpu_temp.sh
+++ cpu_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Uses /sys/class/thermal/thermal_zone0/temp to fetch CPU temperature
-# Displays output in XX.X format. (e.g: 45.7°C) 
+# Displays output in XX format. (e.g: 45°C) 
 
 T=$(cat /sys/class/thermal/thermal_zone0/temp); 
 echo $(( ${T:0:2} ))°C
